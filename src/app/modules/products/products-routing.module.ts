6d9abd850b3c9445cd258ded52f50bd347b197c0
obryl <olehrb29@gmail.com>
2018-07-15 13:12:21 +0300

    products page bootstrap

--- src/app/modules/products/products-routing.module.ts
+++ src/app/modules/products/products-routing.module.ts
@@ -1,7 +1,7 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 
-import {ProductsComponent} from './products/products.component';
+import {ProductsComponent} from './container/products.component';
 
 const routes: Routes = [
     {
