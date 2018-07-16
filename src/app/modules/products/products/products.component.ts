eaa1c5adb15dd540aa44a9c6969e5ea8ffeae6b3
brylkovskyi <brylkovsky@gmail.com>
2018-07-16 17:35:42 +0300

    animations on route change

--- src/app/modules/products/products/products.component.ts
+++ src/app/modules/products/products/products.component.ts
@@ -1,12 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, HostBinding, OnInit} from '@angular/core';
+import {slideInDownAnimation} from '../../../animations';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.scss']
+  styleUrls: ['./products.component.scss'],
+    animations: [slideInDownAnimation]
 })
 export class ProductsComponent implements OnInit {
-
+    @HostBinding('@routeAnimation') routeAnimation = true;
+    @HostBinding('style.display')   display = 'block';
   constructor() { }
 
   ngOnInit() {
