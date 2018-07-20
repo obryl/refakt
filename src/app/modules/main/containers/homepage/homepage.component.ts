babf1f2ac05dccc3bd7761bb24ec3d1ae3ebb637
brylkovskyi <brylkovsky@gmail.com>
2018-07-20 13:08:03 +0300

    migrating to angular 6 homepage design

--- src/app/modules/main/containers/homepage/homepage.component.ts
+++ src/app/modules/main/containers/homepage/homepage.component.ts
@@ -1,19 +1,22 @@
 import {Component, HostBinding, OnInit} from '@angular/core';
 
 import {slideInDownAnimation} from '../../../../animations';
+import {NgbCarouselConfig} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
     selector: 'app-homepage',
     templateUrl: './homepage.component.html',
     styleUrls: ['./homepage.component.scss'],
-    animations: [slideInDownAnimation]
+    animations: [slideInDownAnimation],
+    providers: [NgbCarouselConfig]
 })
 export class HomepageComponent implements OnInit {
 
     @HostBinding('@routeAnimation') routeAnimation = true;
     @HostBinding('style.display')   display = 'block';
 
-    constructor() {
+    constructor(public config: NgbCarouselConfig) {
+
     }
 
     ngOnInit() {
